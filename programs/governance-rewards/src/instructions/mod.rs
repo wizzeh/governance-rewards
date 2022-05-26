0c161033750229cc18bc216da7c2799a238e884c
Ellie High <6687206+Wizzeh@users.noreply.github.com>
2022-05-26 14:42:06 -0700

    Allow updating registration

--- programs/governance-rewards/src/instructions/mod.rs
+++ programs/governance-rewards/src/instructions/mod.rs
@@ -3,9 +3,11 @@ pub mod create_distribution;
 pub mod preferences;
 pub mod reclaim;
 pub mod register;
+pub mod update_registration;
 
 pub use claim::*;
 pub use create_distribution::*;
 pub use preferences::*;
 pub use reclaim::*;
 pub use register::*;
+pub use update_registration::*;
