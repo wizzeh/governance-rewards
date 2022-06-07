f0d9be8b0804fc0195926af1d8c653bb8ad62dd6
Ellie High <6687206+Wizzeh@users.noreply.github.com>
2022-06-07 15:49:16 -0700

    Add claim to client

--- programs/governance-rewards-client/src/lib.rs
+++ programs/governance-rewards-client/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::{prelude::Pubkey, solana_program::instruction::Instruction};
+use governance_rewards::state::distribution::Distribution;
 use governance_rewards::state::{claim_data::ClaimData, preferences::UserPreferences};
 
 pub fn register(
@@ -17,6 +18,38 @@ pub fn register(
             claim_data: ClaimData::get_address(user, distribution),
             registrant: user,
             payer,
+            system_program: solana_sdk::system_program::id(),
+        },
+        None,
+    );
+
+    Instruction {
+        program_id: governance_rewards::id(),
+        accounts,
+        data,
+    }
+}
+
+pub fn claim(
+    user: Pubkey,
+    distribution: Pubkey,
+    realm: Pubkey,
+    rewards_account: Pubkey,
+    to_account: Pubkey,
+    payer: Pubkey,
+) -> Instruction {
+    let data = anchor_lang::InstructionData::data(&governance_rewards::instruction::Claim {});
+    let accounts = anchor_lang::ToAccountMetas::to_account_metas(
+        &governance_rewards::accounts::Claim {
+            caller: payer,
+            claimant: user,
+            distribution,
+            rewards_account,
+            to_account,
+            payout_authority: Distribution::get_payout_authority(distribution),
+            claim_data: ClaimData::get_address(user, distribution),
+            preferences: UserPreferences::get_address(user, realm),
+            token_program: anchor_spl::token::ID,
             system_program: solana_sdk::system_program::id(),
         },
         None,
